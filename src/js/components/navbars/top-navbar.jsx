98c630b13e21c3b62c78c773c5eac266cccc518d
Lawrence Cheung <lawrence0819@gmail.com>
2016-03-19 19:27:31 +0800

    add current username on top navbar

--- src/js/components/navbars/top-navbar.jsx
+++ src/js/components/navbars/top-navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { SideBarIcon, SearchIcon } from '../shared/icons';
 import { Container } from '../shared/containers';
 import { Link } from '../shared/links';
+import profileImage from '../../../assets/steve.jpg';
 
 export default props => (
   <Container>
@@ -16,8 +17,21 @@ export default props => (
           <SearchIcon />
         </div>
       </div>
-      <div className="ui right item" onClick={props.toggleSidebar}>
-        <SideBarIcon />
+      <div className="ui right menu secondary grid">
+        <div className="computer tablet only row">
+          <div className="item account-profile">
+            <img src={profileImage} alt="profile image" />
+            <a>Steve Jobs</a>
+          </div>
+          <div className="item" onClick={props.toggleSidebar}>
+            <SideBarIcon />
+          </div>
+        </div>
+        <div className="mobile only row">
+          <div className="item" onClick={props.toggleSidebar}>
+            <SideBarIcon />
+          </div>
+        </div>
       </div>
     </div>
   </Container>
